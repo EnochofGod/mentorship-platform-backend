0be2efa3a3a3417c9f27cb03a3be1daa38eefc00
ahemforGod <adepateenoch@gmail.com>
2025-07-12 10:03:08 +0100

    fourth commit

--- src/app.js
+++ src/app.js
@@ -1,21 +1,43 @@
 const express = require('express');
-const cors = require('cors');
-const errorHandler = require('./utils/errorHandler');
+const cors = require('cors'); // Import the CORS middleware
+const errorHandler = require('./utils/errorHandler'); // Assuming you have this
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const requestRoutes = require('./routes/requestRoutes');
 const sessionRoutes = require('./routes/sessionRoutes');
 const availabilityRoutes = require('./routes/availabilityRoutes');
 const adminRoutes = require('./routes/adminRoutes');
+
+
 const app = express();
-app.use(cors());
-app.use(express.json());
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://enochofgod.github.io',
+    'https://responsible-bravery.up.railway.app'
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.use(express.json()); 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/requests', requestRoutes);
 app.use('/api/sessions', sessionRoutes);
 app.use('/api/availability', availabilityRoutes);
 app.use('/api/admin', adminRoutes);
+
 app.get('/', (req, res) => {
     res.send('Mentorship Platform Backend API is running!');
 });
