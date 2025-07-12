b230b46f64c233aa796acef5577f64f982edc99c
ahemforGod <adepateenoch@gmail.com>
2025-07-12 10:40:45 +0100

    fifth commit

--- server.js
+++ server.js
@@ -1,35 +1,19 @@
-require('dotenv').config();
+require('dotenv').config(); 
+
 const express = require('express');
 const { Sequelize } = require('sequelize');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const requestRoutes = require('./routes/requestRoutes');
+const sessionRoutes = require('./routes/sessionRoutes');
+const availabilityRoutes = require('./routes/availabilityRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const errorHandler = require('./utils/errorHandler'); 
+
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-
-const allowedOrigins = [
-    'http://localhost:3000', 
-    'https://enochofgod.github.io',
-    'https://responsible-bravery.up.railway.app'
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true,
-  optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions)); 
-
-
-// Database connection
+const PORT = process.env.PORT || 8080; // 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -46,11 +30,37 @@ const sequelize = new Sequelize(
 sequelize.authenticate()
   .then(() => console.log('Database connected!'))
   .catch(err => console.error('Unable to connect to the database:', err));
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://enochofgod.github.io',
+    'https://mentorship-platform-backend-production.up.railway.app' 
+];
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/requests', requestRoutes);
+app.use('/api/sessions', sessionRoutes);
+app.use('/api/availability', availabilityRoutes);
+app.use('/api/admin', adminRoutes);
 app.get('/', (req, res) => {
-  res.send('Mentorship Platform Backend is running!');
+    res.send('Mentorship Platform Backend API is running!');
 });
-
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
